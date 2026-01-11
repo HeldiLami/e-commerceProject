1710ee4646f5e43aeb585765c01b6b92f9caeee0
sildi-sh <sherollisildi@gmail.com>
2026-01-11 15:59:31 +0100

    fix authController (+temp csrf removal on login register)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
@@ -30,8 +30,8 @@ class AuthController extends Controller
             ->letters()
             ->numbers()
             ->symbols(),
-            'is_admin'=>'boolean'
-        ],
+        ], 
+        'is_admin'=>'boolean'
     ]);
 
     $validatedUser['password'] = Hash::make($validatedUser['password']);
