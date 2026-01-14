4f06f119bc885d1e9880b1f9da32690f07bccb72
sildi-sh <sherollisildi@gmail.com>
2026-01-14 13:13:39 +0100

    fixed smth

--- resources/js/amazon.js
+++ resources/js/amazon.js
@@ -1,71 +1,10 @@
 import { addToCart, updateCartQuantity } from "./data/cart.js";
-import { products, loadProductsFetch } from "./data/products.js";
-import { getAssetUrl } from "./utils/assets.js";
+import { loadProductsFetch } from "./data/products.js";
 
-function renderProductsGrid(){
-  let productsHTML='';
-  products.forEach((product)=>{
-    productsHTML+=`
-    <div class="product-container">
-            <div class="product-image-container">
-              <img class="product-image" src="${product.image}">
-            </div>
-
-            <div class="product-name limit-text-to-2-lines">
-              ${product.name}
-            </div>
-
-            <div class="product-rating-container">
-              <img class="product-rating-stars" src="${product.getStarsUrl()}">
-              <div class="product-rating-count link-primary">
-                ${product.rating.count}
-              </div>
-            </div>
-
-            <div class="product-price">
-              ${product.getPrice()}
-            </div>
-
-            <div class="product-quantity-container">
-              <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            ${product.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart">
-              <img src="${getAssetUrl('images/icons/checkmark.png')}">
-              Added
-            </div>
-
-            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-              Add to Cart
-            </button>
-          </div>`
-  });
-  
-  const gridElement = document.querySelector('.js-products-grid');
-  if(gridElement) {
-      gridElement.innerHTML = productsHTML;
-  }
-
-  updateCartQuantity('.js-cart-quantity');
-
+function setupAddToCart() {
   document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-      button.addEventListener('click', ()=>{
+    .forEach((button) => {
+      button.addEventListener('click', () => {
         const productId = button.dataset.productId;
         addToCart(productId);
         updateCartQuantity('.js-cart-quantity');
@@ -74,4 +13,4 @@ function renderProductsGrid(){
 }
 
 await loadProductsFetch();
-renderProductsGrid();
+setupAddToCart();
--- resources/views/front/amazon.blade.php
+++ resources/views/front/amazon.blade.php
@@ -4,6 +4,43 @@
     </x-slot>
 
     <div class="products-grid js-products-grid">
+        @foreach($products as $product)
+            <div class="product-container">
+                <div class="product-image-container">
+                    <img class="product-image" src="{{ asset($product->image) }}">
+                </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                    {{ $product->name }}
+                </div>
+
+                <div class="product-rating-container">
+                    <img class="product-rating-stars" src="/images/ratings/rating-{{ $product->rating_stars * 10 }}.png">
+                    <div class="product-rating-count link-primary">
+                        {{ $product->rating_count }}
+                    </div>
+                </div>
+
+                <div class="product-price">
+                    ${{ number_format($product->price_cents / 100, 2) }}
+                </div>
+
+                <div class="product-quantity-container">
+                    <select class="js-quantity-selector-{{ $product->id }}">
+                        @for ($i = 1; $i <= 10; $i++)
+                            <option value="{{ $i }}">{{ $i }}</option>
+                        @endfor
+                    </select>
+                </div>
+
+                <div class="product-spacer"></div>
+
+                <button class="add-to-cart-button button-primary js-add-to-cart" 
+                        data-product-id="{{ $product->id }}">
+                    Add to Cart
+                </button>
+            </div>
+        @endforeach
     </div>
 
     <x-slot name="scripts">
