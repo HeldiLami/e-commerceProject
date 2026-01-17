b2895cdc6b7460c56fa911f1092d1398cdbb86f5
Neri Sinaj <nerisinaj@gmail.com>
2026-01-17 15:08:50 +0100

    fix add to cart quantity

--- resources/js/amazon.js
+++ resources/js/amazon.js
@@ -1,16 +1,18 @@
 import { addToCart, updateCartQuantity } from "./data/cart.js";
 
 function setupAddToCart() {
-  document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-      button.addEventListener('click', () => {
-        const productId = button.dataset.productId;
-        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-        const quantity = quantitySelector ? Number(quantitySelector.value) : 1;
-        addToCart(productId, quantity);
-        updateCartQuantity('.js-cart-quantity');
-      });
+  const grid = document.querySelector(".js-products-grid");
+  if (!grid) return;
+
+  grid.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId;
+      const quantitySelector = grid.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = quantitySelector ? Number(quantitySelector.value) : 1;
+      addToCart(productId, quantity);
+      updateCartQuantity(".js-cart-quantity");
     });
+  });
 }
 
 setupAddToCart();
