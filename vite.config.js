7ec82beba03a4d78d5d6902afb849e0b00bfa2b5
sildi-sh <sherollisildi@gmail.com>
2026-01-18 20:39:28 +0100

    not working lol

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
         port: 5174,
         strictPort: true,
         hmr: {
-            host: "127.0.0.1",
+            host: "0.0.0.0",
             port: 5174,
         },
         watch: {
