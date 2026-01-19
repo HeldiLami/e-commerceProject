687c3a1c4587d95d80a2198be80161c5a64861f1
HeldiLami <heldilami54@gmail.com>
2026-01-19 16:31:48 +0100

    Komiti i fundit

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        host: "127.0.0.1",
+        host: "0.0.0.0",
         port: 5174,
         strictPort: true,
         hmr: {
